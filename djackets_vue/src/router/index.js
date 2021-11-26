53175f6f6e047abc5e9456ce24ef5df5b3c53fb9
shota-nukumizu <author2000.1225@gmail.com>
2021-11-26 10:44:45 +0900

    カテゴリページ、検索機能実装

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+
 
 const routes = [
   {
@@ -21,6 +24,16 @@ const routes = [
     path: '/:category_slug/:product_slug/',
     name: 'Product',
     component: Product
+  },
+  {
+    path: '/:category_slug',
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
   },
 ]
 
--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -44,18 +44,24 @@ export default {
         this.getProduct()
     },
     methods: {
-        getProduct() {
+        async getProduct() {
+            this.$store.commit('setIsLoading', true)
+
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
+            await axios
                 .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
                     this.product = response.data
+
+                    document.title = this.product.name + ' | Djackets'
                 })
                 .catch(error => {
                     console.log(error)
                 })
+            
+            this.$store.commit('setIsLoading', false)
         },
         addToCart() {
             if (isNaN(this.quantity) || this.quantity < 1) {
