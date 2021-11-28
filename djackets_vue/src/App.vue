7215195fed2d3eac640f84a4ee00365696edb458
shota-nukumizu <author2000.1225@gmail.com>
2021-11-28 14:40:22 +0900

    created Account page

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -37,7 +37,14 @@
 
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/login" class="button is-Light">Log in</router-link>
+
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/my-account" class="button is-light">My Account</router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/login" class="button is-Light">Log in</router-link>
+              </template>
 
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
@@ -7,6 +10,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 
 const routes = [
@@ -34,6 +38,14 @@ const routes = [
     component: Login
   },
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
     path: '/:category_slug/:product_slug/',
     name: 'Product',
     component: Product
@@ -58,6 +70,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'Login', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
